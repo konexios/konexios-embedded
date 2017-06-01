ce749d357ef97725b086663e8efd1b74c450e35f
Danila <ddemidov@mera.ru>
2017-06-01 19:33:24 +0300

    rm axTLS, opt waolSSL

--- BB-GEVK/acnsdkc/ssl/ssl.cpp
+++ BB-GEVK/acnsdkc/ssl/ssl.cpp
@@ -7,35 +7,120 @@
  */
 
 extern "C" {
-#include <axtls-code/ssl/ssl.h>
+//#include <axTLS/ssl/ssl.h>
+#include <wolfssl/ssl.h>
 #include <ssl/ssl.h>
 #include <arrow/mem.h>
+#include <debug.h>
+#include <unint.h>
+#include <bsd/socket.h>
+#ifdef DEBUG_WOLFSSL
+#include <wolfcrypt/logging.h>
+#endif
 
+#if 0
 SSL_CTX *_ssl_ctx = NULL;
 SSL *_ssl = NULL;
 uint8_t session_id[SSL_SESSION_ID_SIZE];
 SSL_EXTENSIONS *ext = NULL;
+#endif
+
+WOLFSSL_METHOD  *method;
+WOLFSSL_CTX     *ctx;
+WOLFSSL         *ssl;
+static int _socket = -1;
+
+void *__real_calloc(size_t num, size_t size);
+void *__wrap_calloc(size_t num, size_t size) {
+	void *p = malloc(num * size);
+//	DBG("calloc %d %d = %p", num, size, p);
+	memset(p, 0x0, num * size);
+	return p;
+}
+
+#ifdef DEBUG_WOLFSSL
+static void cli_wolfSSL_Logging_cb(const int logLevel,
+                                  const char *const logMessage) {
+    DBG("[http]:%d (%s)", logLevel, logMessage);
+}
+#endif
+
+static int recv_ssl(WOLFSSL *wsl, char* buf, int sz, void* vp) {
+    SSP_PARAMETER_NOT_USED(wsl);
+    int *sock = (int *)vp;
+    if ( sz < 0 ) return sz;
+    uint32_t got = 0;
+    got = (uint32_t)recv(_socket, buf, (uint32_t)sz, 0);
+    DBG("recv ssl %d [%d]", got, sz);
+    if (got == 0)  return -2;  // IO_ERR_WANT_READ;
+    return (int)got;
+}
+
+
+static int send_ssl(WOLFSSL *wsl, char* buf, int sz, void* vp) {
+    SSP_PARAMETER_NOT_USED(wsl);
+    int *sock = (int *)vp;
+    if ( sz < 0 ) return sz;
+    uint32_t sent = 0;
+    sent = (uint32_t)send(_socket, buf, (uint32_t)sz, 0);
+    DBG("send ssl %d [%d]", sent, sz);
+    if (sent == 0)
+        return -2;  // IO_ERR_WANT_WRITE
+    return (int)sent;
+}
 
 int ssl_connect(int sock) {
+	wolfSSL_Init();
+	_socket = sock;
+	DBG("init ssl connect %d", sock)
+	method = wolfTLSv1_2_client_method();
+	ctx = wolfSSL_CTX_new(method);
+	if ( ctx == NULL) {
+		DBG("unable to get ctx");
+	}
+	wolfSSL_CTX_set_verify(ctx, SSL_VERIFY_NONE, NULL);
+	wolfSSL_SetIORecv(ctx, recv_ssl);
+	wolfSSL_SetIOSend(ctx, send_ssl);
 
-
+	ssl = wolfSSL_new(ctx);
+	if (ssl == NULL) {
+		DBG("oops, bad SSL ptr");
+	}
+	wolfSSL_SetIOReadCtx(ssl, (void*)&_socket);
+	wolfSSL_SetIOWriteCtx(ssl, (void*)&_socket);
+#ifdef DEBUG_WOLFSSL
+    wolfSSL_SetLoggingCb(cli_wolfSSL_Logging_cb);
+    wolfSSL_Debugging_ON();
+#endif
+	int err = wolfSSL_connect(ssl);
+	if (err != SSL_SUCCESS) {
+		DBG("SSL connect fail");
+		return -1;
+	} else {
+		DBG("SSL connect done");
+	}
+#if 0
+	DBG("ssl_ctx_new");
 	_ssl_ctx = ssl_ctx_new(SSL_SERVER_VERIFY_LATER, SSL_DEFAULT_CLNT_SESS);
 	if ( !_ssl_ctx )	return -1;
 
 //    _ssl.ssl_ctx = &_ssl_ctx;
 
 //	_ssl = ssl_client_new(_ssl_ctx, sock, session_id, SSL_SESSION_ID_SIZE, ext);
+	DBG("ssl_client_new %d", sock);
 	_ssl = ssl_client_new(_ssl_ctx, sock, NULL, 0, ext);
     if( !_ssl ) {
     	return -1;
     }
+    DBG("ssl_handshake %d", ssl_handshake_status(_ssl));
     if(ssl_handshake_status(_ssl) != SSL_OK) {
     	return -1;
     }
+#endif
     return 0;
 }
 
-static char *data_p = NULL;
+/*static char *data_p = NULL;
 int size_p = 0;
 int ssl_recv(int sock, char *data, int len) {
 	if ( !size_p ) {
@@ -55,6 +140,30 @@ int ssl_recv(int sock, char *data, int len) {
 int ssl_send(int sock, char* data, int length) {
     if ( sock < 0 ) return -1;
     return ssl_write(_ssl, (uint8_t*)data, length);
+}*/
+
+
+int ssl_recv(int sock, char *data, int len) {
+	DBG("ssl r[%d]", len);
+	return wolfSSL_read(ssl, data, (int)len);
+}
+
+int ssl_send(int sock, char* data, int length) {
+	DBG("ssl w[%d]", length);
+	return wolfSSL_write(ssl, data, (int)length);
+}
+
+int ssl_close(int sock) {
+	SSP_PARAMETER_NOT_USED(sock);
+	DBG("close ssl");
+	wolfSSL_free(ssl);
+	wolfSSL_CTX_free(ctx);
+	wolfSSL_Cleanup();
+	/*ssl_free(_ssl);
+	DBG("close ssl ctx")
+	ssl_ctx_free(_ssl_ctx);*/
+	DBG("close done");
+	return 0;
 }
 
 } // extern "C"
--- BB-GEVK/main.cpp
+++ BB-GEVK/main.cpp
@@ -38,12 +38,10 @@ W5100Interface eth;
 WizFi250Interface eth;
 #endif
 
-#if 0
 NHD_C0216CZ lcd;
 // Sensors
 NOA1305 als;
 NCS36000 pir;
-#endif
 
 #if 0
 #define INT_SET		1
@@ -59,7 +57,7 @@ void als_isr() {
 }
 #endif
 
-#if 0
+/*
 DLBSmotor rotor;
 static int rotor_cmd(const char *str) {
 	DBG("cmd: [%s]", str);
@@ -75,6 +73,7 @@ static int rotor_cmd(const char *str) {
 	json_delete(_main);
 	return 0;
 }
+*/
 
 Arrow_Motor motor;
 static int motor_rotate(const char *str) {
@@ -129,24 +128,18 @@ static int get_telemetry_data(void *d) {
 #endif
     if ( old_pir_data != data->pir || count++ >= max_count ) {
     	count = 0;
-    	DBG("-%s-", get_api_key());
     	DBG("data PIR(%d), ALS{%d,%4.2f}", data->pir, data->als, data->abmienceInLux);
     	return 0;
     }
     return -1;
 }
-#endif
+
 
 int main() {
-
-#if 0
 	lcd.init();
 	lcd.displayString(TITLE);
-#endif
-
 	wait(1);
 	DBG("==== START =====");
-
 
 #if defined(USE_POE_SHIELD)
 	 	uint8_t mac[6];
@@ -161,10 +154,10 @@ int main() {
 //#endif
 	 	if (!ret) {
 	 		DBG("Initialized, MAC: %s", eth.getMACAddress());
-//	 		lcd.displayString("MAC SET");
+	 		lcd.displayString("MAC SET");
 	 	} else {
 	 		DBG("Error eth.init() - ret = %d", ret);
-//	 		lcd.displayString("ERROR !!!\nMAC NOT SET");
+	 		lcd.displayString("ERROR !!!\nMAC NOT SET");
 	 		return -1;
 	 	}
 
@@ -172,12 +165,12 @@ int main() {
 	 	ret = eth.connect();
 	 	if (!ret) {
 	 		DBG("IP: %s, MASK: %s, GW: %s",eth.getIPAddress(),eth.getNetworkMask(),eth.getGateway());
-//	 		lcd.clearDisplay();
-//	 		lcd.displayStringToPos("IP : ", 1, 1);
-//	 		lcd.displayStringToPos(eth.getIPAddress(), 2, 1);
+	 		lcd.clearDisplay();
+	 		lcd.displayStringToPos("IP : ", 1, 1);
+	 		lcd.displayStringToPos(eth.getIPAddress(), 2, 1);
 	 	}else{
 	 		DBG("Error eth.connect() - ret = %d", ret);
-//	 		lcd.displayString("ERROR !!!\nExiting Demo");
+	 		lcd.displayString("ERROR !!!\nExiting Demo");
 	 		return -1;
 	 	}
 
@@ -210,7 +203,6 @@ int main() {
 	 	}
 #endif
 
-#if 0
 	 	//Initialize ALS
 		if (als.init() != ALS_SUCCESS) {
 			lcd.displayString("ALS initialization failed");
@@ -220,7 +212,6 @@ int main() {
 			DBG("ALS initialization successful");
 			wait(1);
 		}
-#endif
 
 #if 0
 		// Enabled ALS interrupt
@@ -247,11 +238,9 @@ int main() {
 		  lcd.displayString("Light intensity");
 #endif
 
-#if 0
 	 	//Initialize LEDs
 	 	led.init();
-	 	rotor.init();
-#endif
+//	 	rotor.init();
 
 	 	time_t now = time(NULL);
 	 	DBG("test time %d", now);
@@ -261,23 +250,23 @@ int main() {
 	 	/*time_t*/ now = time(NULL);
 	 	DBG("date : %s", ctime(&now));
 
-//	  lcd.displayString("gateway and device connecting...");
+	  lcd.displayString("gateway and device connecting...");
 	  arrow_initialize_routine();
 
 	  gevk_data_t data;
-//	  while( get_telemetry_data(&data) < 0 ) ;
+	  while( get_telemetry_data(&data) < 0 ) ;
 
 	  arrow_send_telemetry_routine(&data);
 
-//	  add_cmd_handler("motor", motor_rotate);
-//	  add_cmd_handler("led", led_on);
+	  add_cmd_handler("motor", motor_rotate);
+	  add_cmd_handler("led", led_on);
 //	  add_cmd_handler("rotor", rotor_cmd);
 
-//      lcd.displayString("mqtt connecting...");
-//      arrow_mqtt_connect_routine();
+      lcd.displayString("mqtt connecting...");
+      arrow_mqtt_connect_routine();
 
-//      lcd.displayString("send telemetry");
-//      arrow_mqtt_send_telemetry_routine(get_telemetry_data, &data);
+      lcd.displayString("send telemetry");
+      arrow_mqtt_send_telemetry_routine(get_telemetry_data, &data);
 
       arrow_close();
 }
