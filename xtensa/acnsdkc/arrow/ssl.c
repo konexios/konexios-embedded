59ebf0e84b890779b96d90b8d482adc2b50376ba
Danila V. Demidow <dandemidow@gmail.com>
2017-09-21 22:06:03 +0300

    add info

--- xtensa/acnsdkc/arrow/ssl.c
+++ xtensa/acnsdkc/arrow/ssl.c
@@ -23,14 +23,14 @@
 
 #define SSL_SUCCESS 0
 
-static SSL_CTX *ctx[2] = {0};
-static SSL *ssl[2] = {0};
+static SSL_CTX *ctx[4] = {0};
+static SSL *ssl[4] = {0};
 
-int sock_num[2] = {-1, -1};
+int sock_num[4] = {-1, -1, -1, -1};
 
 int find_free_sock() {
   int i;
-  for (i=0; i< 2; i++)
+  for (i=0; i< 4; i++)
     if ( sock_num[i] < 0) return i;
   return -1;
 }
@@ -48,27 +48,31 @@ void free_sock(int sock) {
 }
 
 int ssl_connect(int sock) {
-
   int socket_fd = find_free_sock();
-  if ( socket_fd< 0 ) return -1;
+  if ( socket_fd < 0 ) return -1;
   sock_num[socket_fd] = sock;
+
+  DBG("Connect %d, %d", sock, socket_fd);
 
   ctx[socket_fd] = qcom_SSL_ctx_new(SSL_CLIENT, SSL_INBUF_SIZE, SSL_OUTBUF_SIZE, 0);
   if ( ctx[socket_fd] == NULL) {
     DBG("unable to get ctx");
     return -1;
   }
+  DBG("Create ctx %d", socket_fd);
   ssl[socket_fd] = qcom_SSL_new(ctx[socket_fd]);
   qcom_SSL_set_fd(ssl[socket_fd], sock);
   if (ssl[socket_fd] == NULL) {
     DBG("oops, bad SSL ptr");
     return -1;
   }
+  DBG("Create ssl %d", socket_fd);
   int err = qcom_SSL_connect(ssl[socket_fd]);
   if (err < 0) {
     DBG("SSL connect fail");
     return err;
   }
+  DBG("SSL connect done");
   return SSL_SUCCESS;
 }
 
@@ -87,12 +91,14 @@ int ssl_recv(int sock, char *data, int len) {
     if (ret == 0) return (-1);
     else return (ret);
   }
+  DBG("ssl recv %d", len);
   return qcom_SSL_read(ssl[socket_fd], data, len);
 }
 
 int ssl_send(int sock, char* data, int length) {
   int socket_fd = find_sock(sock);
   if ( socket_fd < 0 ) return -1;
+  DBG("ssl send %d", length);
   return qcom_SSL_write(ssl[socket_fd], data, length);
 }
 
@@ -103,7 +109,6 @@ int ssl_close(int sock) {
   if ( ssl[socket_fd] ) qcom_SSL_shutdown(ssl[socket_fd]);
   if ( ctx[socket_fd] ) qcom_SSL_ctx_free(ctx[socket_fd]);
   free_sock(sock);
-  DBG("xcc close done");
   return 0;
 }
 
