d2592251945b2bdadc3a7f8078fe3cb18519530d
Danila Demidov <ddemidov@mera.ru>
2017-09-28 15:34:53 +0300

    xtensa: rm qcom ssl, add wolfSSL

---
+++ xtensa/arrow_wolf_mem.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2017 Arrow Electronics, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License 2.0
+ * which accompanies this distribution, and is available at
+ * http://apache.org/licenses/LICENSE-2.0
+ * Contributors: Arrow Electronics, Inc.
+ */
+
+#include <arrow_wolf_mem.h>
+#include <arrow/mem.h>
+#include <debug.h>
+
+void *XMALLOC(size_t n, void* heap, int type) {
+    SSP_PARAMETER_NOT_USED(heap);
+    SSP_PARAMETER_NOT_USED(type);
+    void *p = malloc(n);
+    if ( !p ) DBG("No alloc memory");
+    return p;
+}
+void *XREALLOC(void *p, size_t n, void* heap, int type) {
+    SSP_PARAMETER_NOT_USED(heap);
+    SSP_PARAMETER_NOT_USED(type);
+    void *m = realloc(p, n);
+    if ( !m ) DBG("No alloc memory");
+    return m;
+}
+void XFREE(void *p, void* heap, int type) {
+    SSP_PARAMETER_NOT_USED(heap);
+    SSP_PARAMETER_NOT_USED(type);
+    if ( p ) free(p);
+}
+
+int rand() {
+    int _rand;
+    qcom_crypto_rng_get(&_rand, 2);
+
+    return _rand;
+}
