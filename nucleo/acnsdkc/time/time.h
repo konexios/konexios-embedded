293a42bc61397e781ec3904c21f06dec5f8f028b
Danila Demidov <ddemidov@mera.ru>
2017-11-02 16:51:24 +0300

    nucleo: pltf files is added

---
+++ nucleo/acnsdkc/time/time.h
@@ -0,0 +1,19 @@
+/* Copyright (c) 2017 Arrow Electronics, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License 2.0
+ * which accompanies this distribution, and is available at
+ * http://apache.org/licenses/LICENSE-2.0
+ * Contributors: Arrow Electronics, Inc.
+ */
+
+#ifndef ARROW_TIME_TIME_H_
+#define ARROW_TIME_TIME_H_
+
+#include <config.h>
+#define _XOPEN_SOURCE
+#include <time.h>
+#include <sys/time.h>
+#include <platforms/default/time/time.h>
+#include <mbed/platform/mbed_rtc_time.h>
+
+#endif // ARROW_TIME_TIME_H_
