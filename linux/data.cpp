c2d77ee43d523e38d215c2a8d18b2d4ddfefc628
Tam Nguyen <tam@konexios.com>
2022-05-02 21:55:54 -0500

    some code cleanup

--- linux/data.cpp
+++ linux/data.cpp
@@ -2,77 +2,13 @@
 #include <json/telemetry.h>
 #include <iostream>
 #include <time/time.h>
-#if defined(__probook_4540s__)
-#include <sensors/sensors.h>
-#include <json/probook.h>
-#else
 #include <stdlib.h>
 #include <json/pm.h>
-#endif
-
 
 int get_telemetry_data(void *d) {
-#if defined(__probook_4540s__)
-  probook_data_t *data = (probook_data_t *)d;
-    // init the sensors (libsensors4-devel)
-    int ret = sensors_init( NULL );
-    if ( ret ) {
-      std::cerr<<"sensors init failed"<<std::endl;
-      return 1;
-    }
-
-    const sensors_chip_name *chip;
-    const sensors_feature *feature;
-    const sensors_subfeature *sub0;
-    const sensors_subfeature *sub1;
-    int chip_number = 2;
-
-    
-    // now find the temperature sensors on my laptop!
-    chip = sensors_get_detected_chips(NULL, &chip_number);
-
-    char chip_name[512];
-    sensors_snprintf_chip_name(chip_name, 512, chip);
-    std::cout<<"chip " << chip_name << " - " << chip_number << std::endl;
-
-    int feature_number = 1;
-    feature = sensors_get_features(chip, &feature_number);
-    if ( feature->type != SENSORS_FEATURE_TEMP ) {
-      std::cerr << "check SENSORS_FEATURE_TEMP fail" << std::endl;
-      return -1;
-    }
-
-    sub0 = sensors_get_subfeature(chip, feature, SENSORS_SUBFEATURE_TEMP_INPUT);
-    if ( ! sub0->flags & SENSORS_MODE_R ) {
-      std::cerr << "no R mode - core 0"<<std::endl;
-      return -1;
-    }
-
-    feature_number = 2;
-    feature = sensors_get_features(chip, &feature_number);
-    if ( feature->type != SENSORS_FEATURE_TEMP ) return -1;
-    sub1 = sensors_get_subfeature(chip, feature, SENSORS_SUBFEATURE_TEMP_INPUT);
-    if ( ! sub1->flags & SENSORS_MODE_R ) {
-      std::cerr << "no R mode - core 1"<< std::endl;
-      return -1;
-    }
-    
-    // sub0 and sub1 - is the termerature sensors
-    // we can get a temperature value by sensors_get_value()
-    double tmp;
-    sensors_get_value(chip, sub0->number, &tmp);
-    data->temperature_core0 = tmp;
-    sensors_get_value(chip, sub1->number, &tmp);
-    data->temperature_core1 = tmp;
-    std::cout<<"mqtt publish: T("<<data->temperature_core0
-            <<", "<<data->temperature_core1<<")..."<<std::endl;
-#else
   pm_data_t *data = (pm_data_t *)d;
   data->pm_2_5 = rand()%5 + 25;
   data->pm_10 = rand()%5 + 30;
-  std::cout<<"mqtt publish: T("<<data->pm_2_5
-          <<", "<<data->pm_10<<")..."<<std::endl;
   msleep(1000);
-#endif
   return 0;
 }
--- linux/main.cpp
+++ linux/main.cpp
@@ -1,11 +1,3 @@
-/* Copyright (c) 2017 Arrow Electronics, Inc.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Apache License 2.0
- * which accompanies this distribution, and is available at
- * http://apache.org/licenses/LICENSE-2.0
- * Contributors: Arrow Electronics, Inc.
- */
-
 extern "C" {
 #include <konexios_config.h>
 #include <ntp/ntp.h>
@@ -18,11 +10,7 @@ extern "C" {
 #include <ssl/crypt.h>
 #include <konexios/utf8.h>
 #include <konexios/state.h>
-#if defined(__probook_4540s__)
-#include <json/probook.h>
-#else
 #include <json/pm.h>
-#endif
 
 #include <konexios/routine.h>
 #include <konexios/api/device/device.h>
@@ -40,21 +28,15 @@ struct timeval init_time;
 
 #include <iostream>
 
-#if !defined(NO_SOFTWARE_RELEASE)
 extern "C" int konexios_release_download_payload(const char *payload, int size, int);
 extern "C" int konexios_release_download_complete(int);
-#endif
 
-#if !defined(NO_SOFTWARE_UPDATE)
 extern "C" int konexios_software_update(const char *url,
                                  const char *checksum,
                                  const char *from,
                                  const char *to);
-#endif
 
 extern int get_telemetry_data(void *data);
-
-#if !defined(NO_EVENTS)
 
 void show_json_obj(JsonNode *o) {
     JsonNode *tmp = o;
@@ -102,72 +84,22 @@ static int fail_cmd_proc(property_t payload) {
   printf("fail: [%s]", P_VALUE(payload));
   return -1;
 }
-#endif
-
-#if defined(ARROW_THREAD)
-static void *thread_telemetry(void *arg) {
- int *telemetry_act = (int *)arg;
- printf("Telemetry thread start\r\n");
- *telemetry_act = 1;
- pm_data_t data;
- konexios_mqtt_connect_telemetry_routine();
- int ret = konexios_mqtt_telemetry_routine(get_telemetry_data, &data);
- switch ( ret ) {
- default:
-     printf("Telemetry thread stop %d\r\n", ret);
-     *telemetry_act = 0;
- }
- konexios_mqtt_disconnect_telemetry_routine();
- pthread_exit(NULL);
- return NULL;
-}
-
-static void *thread_command(void *arg) {
- printf("Command thread start\r\n");
- int *telemetry_act = (int *)arg;
- konexios_mqtt_connect_event_routine();
- konexios_routine_error_t ret = ROUTINE_SUCCESS;
- while( *telemetry_act && ret == ROUTINE_SUCCESS ) {
-      ret = konexios_mqtt_event_receive_routine();
- }
- konexios_mqtt_disconnect_event_routine();
- pthread_exit(NULL);
- return NULL;
-}
-
-static void *thread_cmd_proc(void *arg) {
- printf("Command process start\r\n");
- int *telemetry_act = (int *)arg;
- while ( *telemetry_act ) {
-     if ( konexios_mqtt_event_proc() < 0 ) msleep(1000);
- }
- pthread_exit(NULL);
- return NULL;
-}
-#endif
 
 int main() {
     std::cout<<std::endl<<"--- Demo Linux ---"<<std::endl;
-
-//    ntp_set_time_cycle();
-    
+  
     time_t ctTime = time(NULL);
     std::cout<<"Time is set to (UTC): "<<ctime(&ctTime)<<std::endl;
 
-    // start Arrow Connect
     if ( konexios_init() < 0 ) return -1;
 
-#if !defined(NO_SOFTWARE_UPDATE)
-    //konexios_software_release_set_cb(&konexios_software_update);
-#endif
+    konexios_software_release_set_cb(&konexios_software_update);
 
-#if !defined(NO_SOFTWARE_RELEASE)
     konexios_software_release_dowload_set_cb(
                 NULL,
                 konexios_release_download_payload,
                 konexios_release_download_complete,
                 NULL);
-#endif
 
 #if !defined(NO_EVENTS)
 
@@ -186,47 +118,16 @@ do {
         msleep(TELEMETRY_DELAY);
     }
     konexios_startup_sequence(0);
-    #if defined(__probook_4540s__)
-    probook_data_t d;
-    #else
+
     pm_data_t d;
-    #endif
     get_telemetry_data(&d);
     konexios_send_telemetry_routine(&d);
     std::cout<<"send telemetry via API"<<std::endl;
 
-#if defined(ARROW_THREAD)
-    int telemetry_act = 0;
-    pthread_attr_t thread_attr;
-    pthread_attr_init(&thread_attr);
-    pthread_attr_setdetachstate(&thread_attr, PTHREAD_CREATE_DETACHED);
-
-    pthread_t telemetrythread;
-    int result = pthread_create(&telemetrythread, &thread_attr, thread_telemetry, (void*)&telemetry_act);
-    if(result != 0) {
-        printf("Creating thread false. Error: %d\nPress enter to exit\n", result);
-    }
-    pthread_t commandthread;
-    result = pthread_create(&commandthread, NULL, thread_command, (void*)&telemetry_act);
-    if(result != 0) {
-        printf("Creating thread false. Error: %d\nPress enter to exit\n", result);
-    }
-    pthread_t commandprocthread;
-    result = pthread_create(&commandprocthread, NULL, thread_cmd_proc, (void*)&telemetry_act);
-    if(result != 0) {
-        printf("Creating thread false. Error: %d\nPress enter to exit\n", result);
-    }
-    pthread_join(telemetrythread, NULL);
-    pthread_join(commandthread, NULL);
-    pthread_join(commandprocthread, NULL);
-#else
     int mqtt_routine_act = 1;
     konexios_mqtt_connect_routine();
     while ( mqtt_routine_act ) {
-#if !defined(NO_EVENTS)
         if ( konexios_mqtt_has_events() ) {
-            printf("--------- postponed msg start -----------\r\n");
-//            konexios_mqtt_disconnect_routine();
             while ( konexios_mqtt_has_events() ) {
                 struct timeval diff;
                 struct timeval now;
@@ -245,14 +146,11 @@ do {
                        diff.tv_usec);
             }
             printf("--------- postponed msg end -----------\r\n");
-//            konexios_mqtt_connect_routine();
         }
-#endif
+
         int ret = konexios_mqtt_send_telemetry_routine(get_telemetry_data, &d);
         switch ( ret ) {
-#if !defined(NO_EVENTS)
         case ROUTINE_RECEIVE_EVENT:
-//            konexios_mqtt_disconnect_routine();
             while ( konexios_mqtt_has_events() ) {
                 struct timeval now;
                 struct timeval diff;
@@ -272,19 +170,14 @@ do {
                        diff.tv_usec);
             }
             break;
-#endif
-#if defined(VALGRIND_TEST)
-        case ROUTINE_TEST_DONE:
-                mqtt_routine_act = 0;
-            break;
-#endif
+
         default:
-            konexios_mqtt_disconnect_routine();
-            konexios_mqtt_connect_routine();
+            // konexios_mqtt_disconnect_routine();
+            // konexios_mqtt_connect_routine();
             break;
         }
     }
-#endif
+
     konexios_close();
 } while(ttt++ < 3);
     konexios_deinit();
@@ -292,65 +185,3 @@ do {
     std::cout<<std::endl<<"End"<<std::endl;
     return 0;
 }
-/*
-extern "C" {
-#include <malloc.h>
-int m_counter = 0;
-//int f_counter = 0;
-int alloc_size = 0;
-//int free_size = 0;
-
-void *__real_malloc(size_t);
-void *__wrap_malloc(size_t p) {
-  m_counter ++;
-  void *pt = __real_malloc(p);
-  int len = malloc_usable_size(pt);
-  alloc_size += len;
-  printf("-=[syscall]:alloc=- [%d] %d/%d\r\n", m_counter, len, alloc_size);
-  return pt;
-}
-
-void *__real_realloc(void *, size_t);
-void *__wrap_realloc(void *p, size_t s) {
-//  m_counter++;
-  int l = malloc_usable_size(p);
-  alloc_size -= l;
-  void *pt = __real_realloc(p, s);
-  int len = malloc_usable_size(pt);
-  alloc_size += len;
-  printf("-=[syscall]:realloc=- [%d] %d/%d\r\n", m_counter, len, alloc_size);
-  return pt;
-}
-
-void *__real_calloc(size_t, size_t);
-void *__wrap_calloc(size_t p, size_t s) {
-  m_counter++;
-  void *pt = __real_calloc(p, s);
-  int len = malloc_usable_size(pt);
-  alloc_size += len;
-  printf("-=[syscall]:calloc=- [%d] %d/%d\r\n", m_counter, len, alloc_size);
-  return pt;
-}
-
-char *__real_strdup(const char *s);
-char *__wrap_strdup(const char *s) {
-  m_counter++;
-  char *pt = __real_strdup(s);
-  int len = malloc_usable_size(pt);
-  alloc_size += len;
-  printf("-=[syscall]:strdup=- [%d] %d/%d\r\n", m_counter, len, alloc_size);
-  return pt;
-}
-
-char *__real_strndup(const char *s, size_t n);
-char *__wrap_strndup(const char *s, size_t n) {
-  m_counter++;
-  char *pt = __real_strndup(s, n);
-  int len = malloc_usable_size(pt);
-  alloc_size += len;
-  printf("-=[syscall]:strndup=- [%d] %d/%d\r\n", m_counter, len, alloc_size);
-  return pt;
-}
-
-}
-*/
